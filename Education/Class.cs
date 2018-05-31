9ee10e599f55976fea8f7d181139f41d26ddb40c
Anna Miller <annaoberlandermiller@gmail.com>
2018-05-31 16:20:34 -0400

    Has student, instructor, class (with list) and program tabs

---
+++ Education/Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education
+{
+	class Class
+	{
+		public string Name { get; set; }
+		public string Language { get; set; }  //CSharp or Java
+		public string Code { get; set; }
+		public int Capacity { get; set; }
+		public Instructor Instructor { get; set; } //The first Instructor the type!
+		public List<Student> Students = new List<Student>();  //This is a list - unlike an array, we don't have set a fixed amount of data sets
+
+	}
+}
--- Education/Program.cs
+++ Education/Program.cs
@@ -10,6 +10,38 @@ namespace Education
 	{
 		static void Main(string[] args)
 		{
+			Instructor danTheMan = new Instructor(); //this is an instance
+			danTheMan.Name = "Dan";
+			danTheMan.ExperienceInYears = 17;
+			danTheMan.PrimaryLanguage = "CSharp";
+			danTheMan.Email = "danTheMan@military.gov";
+			danTheMan.PhoneNumber = "123-456-7890";
+			
+			Class dotnetbc4 = new Class();
+			dotnetbc4.Capacity = 16;
+			dotnetbc4.Code = "DN4";
+			dotnetbc4.Language = "CSharp";
+			dotnetbc4.Name = "Dot Net Boot Camp 4";
+			dotnetbc4.Instructor = danTheMan;
+
+			Student foster = new Student();
+			foster.Name = "Foster";
+			foster.Absences = 0;
+			foster.ContactInfo = "Call Foster...";
+			foster.FirstJob = DateTime.MinValue; //FirstJob's data type is DateTime. Foster doesn't have a job yet. We can't put null.
+			foster.Class = dotnetbc4;
+
+			Student jj = new Student();
+			jj.Name = "Jonathan";
+			jj.Absences = 0;
+			jj.ContactInfo = "Call Jon";
+			jj.FirstJob = DateTime.MinValue;
+			jj.Class = dotnetbc4;
+
+			dotnetbc4.Students.Add(foster); //this is adding Foster to the list
+			dotnetbc4.Students.Add(jj);
+
+
 		}
 	}
 }
